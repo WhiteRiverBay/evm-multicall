5171bfe98eb0f2f8cdf1da30cafea2d1fd841404
zhangyuxin <2269421+ethanzhrepo@users.noreply.github.com>
2024-12-25 18:41:34 -0800

    update test script

--- test/Multicall.t.sol
+++ test/Multicall.t.sol
@@ -5,6 +5,19 @@ import {Test, console} from "forge-std/Test.sol";
 import {Multicall, Call} from "../src/Multicall.sol";
 import {console2} from "forge-std/console2.sol";
 
+// Mock ERC20 balanceOf
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+}
+
+contract MockERC20 is IERC20 {
+    mapping(address => uint256) public override balanceOf;
+
+    function setBalance(address account, uint256 balance) external {
+        balanceOf[account] = balance;
+    }
+}
+
 contract MulticallTest is Test {
     Multicall public multicall;
 
@@ -35,4 +48,19 @@ contract MulticallTest is Test {
         assertEq(abi.decode(results[0], (uint256)), 1000);
         assertEq(abi.decode(results[1], (uint256)), 2000);
     }
+
+    function test_Multicall_ERC20() public {
+        MockERC20 token = new MockERC20();
+        token.setBalance(address(1), 1000);
+        token.setBalance(address(2), 2000);
+
+        Call[] memory calls = new Call[](2);
+        calls[0] = Call(address(token), abi.encodeWithSelector(token.balanceOf.selector, address(1)));
+        calls[1] = Call(address(token), abi.encodeWithSelector(token.balanceOf.selector, address(2)));
+
+        (, bytes[] memory results) = multicall.multicall(calls);
+
+        assertEq(abi.decode(results[0], (uint256)), 1000);
+        assertEq(abi.decode(results[1], (uint256)), 2000);  
+    }   
 }
