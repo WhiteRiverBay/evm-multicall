fb5629dfa504b012a998edc9bcef038127aeff90
zhangyuxin <2269421+ethanzhrepo@users.noreply.github.com>
2024-12-25 18:15:15 -0800

    update test case

--- src/Multicall.sol
+++ src/Multicall.sol
@@ -1,23 +1,27 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+
+struct Call {
+        address target;
+    bytes callData;
+}
+
 contract Multicall {
     function multicall(
-        bytes[] calldata data
-    ) external returns (bytes[] memory results) {
-        results = new bytes[](data.length);
-        for (uint256 i = 0; i < data.length; i++) {
-            (bool success, bytes memory result) = address(this).delegatecall(
-                data[i]
+        Call[] calldata calls
+    ) external returns (uint256 blockNumber, bytes[] memory results) {
+        blockNumber = block.number;
+        results = new bytes[](calls.length);
+        for (uint256 i = 0; i < calls.length; i++) {
+            (bool success, bytes memory result) = calls[i].target.call(
+                calls[i].callData
             );
-            require(success, "Multicall: delegatecall failed");
+            require(success, "Multicall: call failed");
             results[i] = result;
         }
     }
 
-    function getBalance(address addr) external view returns (uint256) {
-        return addr.balance;
-    }
 
     function getBlockHash(
         uint256 blockNumber
--- test/Multicall.t.sol
+++ test/Multicall.t.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Multicall} from "../src/Multicall.sol";
+import {Multicall, Call} from "../src/Multicall.sol";
+import {console2} from "forge-std/console2.sol";
 
 contract MulticallTest is Test {
     Multicall public multicall;
@@ -12,9 +13,26 @@ contract MulticallTest is Test {
     }   
 
     function test_Multicall() public {
-        bytes[] memory data = new bytes[](1);
-        data[0] = abi.encodeWithSelector(Multicall.getBalance.selector, address(this));
-        bytes[] memory results = multicall.multicall(data);
-        assertEq(abi.decode(results[0], (uint256)), address(this).balance);
+        address addr = address(1);
+        address addr2 = address(2);
+
+        // Start prank for addr
+        vm.startPrank(addr);
+        vm.deal(addr, 1000);
+        vm.stopPrank(); // Stop prank after the transaction
+
+        // Start prank for addr2
+        vm.startPrank(addr2);
+        vm.deal(addr2, 2000);
+        vm.stopPrank(); // Stop prank after the transaction
+
+        Call[] memory calls = new Call[](2);
+        calls[0] = Call(address(multicall), abi.encodeWithSelector(multicall.getEthBalance.selector, addr));
+        calls[1] = Call(address(multicall), abi.encodeWithSelector(multicall.getEthBalance.selector, addr2));
+
+        (, bytes[] memory results) = multicall.multicall(calls);
+
+        assertEq(abi.decode(results[0], (uint256)), 1000);
+        assertEq(abi.decode(results[1], (uint256)), 2000);
     }
 }
